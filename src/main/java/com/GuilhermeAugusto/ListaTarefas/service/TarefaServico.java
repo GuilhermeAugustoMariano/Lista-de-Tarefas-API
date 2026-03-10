545899422e5849693010c4d449111b113cde6a88
Guilherme <guibaaugust067@gmail.com>
2026-03-10 14:54:45 -0300

    Definição do Domínio de Serviço

---
+++ src/main/java/com/GuilhermeAugusto/ListaTarefas/service/TarefaServico.java
@@ -0,0 +1,38 @@
+package com.GuilhermeAugusto.ListaTarefas.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.GuilhermeAugusto.ListaTarefas.entity.Tarefa;
+import com.GuilhermeAugusto.ListaTarefas.repository.TarefaRepositorio;
+
+@Service
+public class TarefaServico {
+  private final TarefaRepositorio tarefaRepositorio;
+
+  public TarefaServico(TarefaRepositorio tarefaRepositorio) {
+    this.tarefaRepositorio = tarefaRepositorio;
+  }
+
+  public List<Tarefa> criar(Tarefa tarefa) {
+    tarefaRepositorio.save(tarefa);
+    return listar();
+  }
+
+  public List<Tarefa> listar() {
+    Sort sort = Sort.by("prioridade").descending().and(Sort.by("nome").ascending());
+    return tarefaRepositorio.findAll(sort);
+  }
+
+  public List<Tarefa> atualizar(Tarefa tarefa) {
+    tarefaRepositorio.save(tarefa);
+    return listar();
+  }
+
+  public List<Tarefa> deletar(Long id) {
+    tarefaRepositorio.deleteById(id);
+    return listar();
+  }
+}
